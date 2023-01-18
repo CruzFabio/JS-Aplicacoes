7f250b691d229f3240ca13fa1a73679680c8ada4
Fabio Cruz <cruz.fabiocorrea@gmail.com>
2023-01-18 17:06:42 -0300

    Atualizei o nome da pasta de icon para icons

--- script.js
+++ script.js
@@ -16,10 +16,20 @@ function displayMessage(msgText, msgType) {
     closeBtn.onclick = function() {
     panel.parentNode.removeChild(panel);
     }
+    if (msgType === 'warning') {
+        msg.style.backgroundImage = 'url(icons/warning.png)';
+        panel.style.backgroundColor = 'red';
+        } else if (msgType === 'chat') {
+        msg.style.backgroundImage = 'url(icons/chat.png)';
+        panel.style.backgroundColor = 'aqua';
+        } else {
+        msg.style.paddingLeft = '20px';
+        }
 }
 
 var btn = document.querySelector('button');
 btn.onclick = function() {
-    displayMessage('Woo, this is a different message!');
+    // displayMessage('Your inbox is almost full — delete some mails', 'warning');
+    displayMessage('Brian: Hi there, how are you today?','chat');
 };  
 
